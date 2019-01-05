bba58c59c25ccff25d31605a2de52f6f48b4bded
slwu89 <slwu89@berkeley.edu>
2019-01-05 20:09:14 +0800

    work out a first-order upwind scheme for von foerester PDE

---
+++ age-structured/basic_upwind.R
@@ -0,0 +1,66 @@
+################################################################################
+# 
+#   Testing a basic first-order upwind scheme for McKendrick-Von Foerster equation
+#   see https://en.wikipedia.org/wiki/Von_Foerster_equation for details.
+#   Using implementation described in:
+#   Abia, L. M., Angulo, O., & Lòpez-Marcos, J. C. (2002). Numerical Solution of Stuctured Population Models, (May 2014).
+# 
+################################################################################
+
+rm(list=ls());gc()
+
+# maximum size of mesh (all time units in years)
+T_max <- 2
+A_max <- 80
+
+# size of mesh cells for finite differences method
+dt <- 1/365
+da <- 1/365
+
+# number of cells in mesh
+# t_cells_n <- T_max/dt
+# a_cells_n <- A_max/da
+
+t_cells <- seq(from=0,to=T_max,by=dt)
+t_cells_n <- length(t_cells)
+
+a_cells <- seq(from=0,to=A_max,by=da)
+a_cells_n <- length(a_cells)
+
+# the solution mesh
+U <- matrix(0,nrow = t_cells_n+1,ncol = a_cells_n)
+
+# initial density
+n <- 1e5
+u0 <- dnorm(x = a_cells,mean = 20,sd = 5)
+u0 <- (u0*n)/sum(u0)
+
+U[1,] <- u0
+v <- dt/da
+
+# fertility function
+beta <- function(U,t){
+  N <- sum(U[t,])
+  return((1/52)*N)
+}
+
+# mortality function
+mu <- function(a,t){
+  return(1/52)
+}
+
+# run simulation
+pb <- txtProgressBar(min = 1,max = (t_cells_n-1))
+for(t in 2:(t_cells_n-1)){
+  
+  a <- 1 # for births
+  U[t,a] <- beta(U,t)
+  
+  for(a in 2:a_cells_n){
+    
+    U[t+1,a] <- U[t,a] - (v*(U[t,a] - U[t,(a-1)])) - (dt*mu(a,t)*U[t,a])
+    
+  }
+  
+  setTxtProgressBar(pb,t)
+}
